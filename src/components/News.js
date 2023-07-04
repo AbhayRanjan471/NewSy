ef0e6f7c1b0f3553a4df7a654773d0d77aa38c49
Abhay Ranjan <abhayranjan471@gmail.com>
2023-07-04 18:28:41 +0530

    till lecture-24 , added card components from the bootstrap , to design the layout

--- src/components/News.js
+++ src/components/News.js
@@ -1,15 +1,27 @@
-import React, { Component } from 'react'
-import NewsItem from '../NewsItem'
+import React, { Component } from "react";
+import NewsItem from "../NewsItem";
 
 export class News extends Component {
   render() {
     return (
-      <div>
-        This is a new Componenet
-        <NewsItem></NewsItem>
+      <div className="container my-3">
+        <h2>NewSy- Top Headline</h2>
+        {/* Cretaing a row */}
+        <div className="row">
+            {/* Cretaing some column inside the column to present the card */}
+          <div className="col-md-4">
+            <NewsItem title="my title" description="mydesc" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="my title" description="mydesc" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="my title" description="mydesc" />
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default News
+export default News;
