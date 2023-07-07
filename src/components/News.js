54648241fc453b95a49e2746b54a8fc096bcb2a4
Abhay Ranjan <abhayranjan471@gmail.com>
2023-07-07 17:09:46 +0530

    lecture-28 , added next nd prev button

--- src/components/News.js
+++ src/components/News.js
@@ -10,7 +10,8 @@ export class News extends Component {
     console.log("Hello I m a constructor from News component");
     this.state ={
       articles: [],
-      loading: false
+      loading: false,
+      page: 1
     }
   }
 
@@ -18,15 +19,50 @@ export class News extends Component {
   async componentDidMount(){
     console.log("componentDidMount");
     //this url is used to fetch the api
-    let url = "https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=8e01be2e8ee9423a9fc0875c304e100f";
-    //using the facth api , which return a 'promise' so we will use async - await
+    let url = "https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=8e01be2e8ee9423a9fc0875c304e100f&page=1&pageSize=10";
+    //using the facth api , which return a 'promise' so we will use async - await and here we are fatcing the data from the url using fetch()
     let data = await fetch(url); // async function will wait till for this promise to complete its work and then it will give its data
     //we have to parse the data to json
     let parsedData = await data.json();
     console.log(parsedData);
     //Now we will set the article with the parseData using this.setState
-    this.setState({articles: parsedData.articles})
+    this.setState({
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults
+    })
 
+  }
+
+   handlePrevClick = async()=>{
+    console.log("previous");
+
+    let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=8e01be2e8ee9423a9fc0875c304e100f&page=${this.state.page-1}&pageSize=10`; 
+    let data = await fetch(url); // async function will wait till for this promise to complete its work and then it will give its data
+    let parsedData = await data.json();  
+    
+    this.setState({
+      articles: parsedData.articles,
+      page: this.state.page-1
+    })
+   
+  }
+   handleNextClick = async()=>{
+    console.log("next");
+
+    if(this.state.page+1 > Math.ceil(this.state.totalResults/20)){
+
+    }
+    else{
+      let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=8e01be2e8ee9423a9fc0875c304e100f&page=${this.state.page+1}&pageSize=20`; 
+      let data = await fetch(url); // async function will wait till for this promise to complete its work and then it will give its data
+      let parsedData = await data.json();  
+      
+      this.setState({
+        articles: parsedData.articles,
+        page: this.state.page+1
+      })
+    }
+   
   }
 
   render() {
@@ -47,8 +83,11 @@ export class News extends Component {
                     {element.urlToImage} newsUrl={element.url}/>
                   </div>
            })}
-            
-           
+                      
+        </div>
+        <div className="container d-flex justify-content-between">
+        <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previous</button>
+        <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
         </div>
       </div>
     );
